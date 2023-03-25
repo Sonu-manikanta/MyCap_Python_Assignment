ecdcc33ef5896403139e2e49dc42d16bc4c335c7
Sonu-manikanta <128914238+Sonu-manikanta@users.noreply.github.com>
2023-03-25 22:52:11 +0530

    radius of the circle.py

---
+++ radius of the circle.py
@@ -0,0 +1,8 @@
+#taking input from the user
+radius = float(input("Enter the radius of the cricle: "))
+#calculating area of circle
+area = 3.14159 * radius ** 2
+print(f"The area of the circle with {radius} is {area}")
+output:
+  Enter the radius of the cricle: 1.1
+The area of the circle with 1.1 is 3.8013239000000003
